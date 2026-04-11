9c6e3abaaaa7f4d8ff013e57ce4642ab421411b8
dancin782-cyber <dancin782@gmail.com>
2026-04-11 17:25:00 +0530

    Update wl.js

--- commands/wl.js
+++ commands/wl.js
@@ -1,29 +1,35 @@
 const Whitelist = require('../models/Whitelist');
 
 module.exports = {
-    name: 'wl',
+    name: "wl",
     async execute(message, args) {
 
         const user = message.mentions.users.first();
-        if (!user) return message.reply("Mention user");
+        if (!user) return message.reply("❌ Mention user");
 
         let data = await Whitelist.findOne({ userId: user.id });
 
         if (!data) {
-            data = new Whitelist({ userId: user.id });
+            data = await Whitelist.create({ userId: user.id });
         }
 
-        data.fullAccess = true;
-
+        // ENABLE ALL FEATURES
         data.features = {
-            mention: true,
+            ban: true,
+            kick: true,
+            roleDelete: true,
+            roleCreate: true,
+            channelDelete: true,
+            channelCreate: true,
             webhook: true,
-            backup: true,
-            security: true
+            mention: true,
+            emoji: true,
+            link: true,
+            spam: true
         };
 
         await data.save();
 
-        message.reply(`${user} now has FULL whitelist`);
+        message.reply(`✅ ${user.tag} is now FULLY WHITELISTED`);
     }
 };
